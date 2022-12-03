7becbce983c8f571346806e262cde0a5c7f5554d
Franco <franco.fantillo@gmail.com>
2022-12-02 21:20:03 -0800

    Initial Commit

---
+++ Workday/WorkdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkdayApp.swift
+//  Workday
+//
+//  Created by Franco Fantillo on 2022-12-02.
+//
+
+import SwiftUI
+
+@main
+struct WorkdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
