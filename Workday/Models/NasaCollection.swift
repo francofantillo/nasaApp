e3660d572ffc7643248e3e3faee1ad4b58c18798
Franco <franco.fantillo@gmail.com>
2022-12-08 01:00:55 +1300

    Cleaned up model data order.

--- Workday/Models/NasaCollection.swift
+++ Workday/Models/NasaCollection.swift
@@ -23,19 +23,20 @@ struct NasaCollection: Decodable {
     let items: [NasaItem]
     let links: [CollectonLinks]?
     
-    init(href: String, links: [CollectonLinks]?, items: [NasaItem]) {
+    init(href: String, items: [NasaItem], links: [CollectonLinks]?) {
+        
         self.href = href
         self.links = links
         self.items = items
     }
     
     init(from decoder: Decoder) throws {
-        // unload the top level
+
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let collectionContainer = try container.nestedContainer(keyedBy: CollectionKeys.self, forKey: .collection)
         let href = try collectionContainer.decode(String.self, forKey: .href)
-        let links = try collectionContainer.decodeIfPresent([CollectonLinks].self, forKey: .links)
         let items = try collectionContainer.decode([NasaItem].self, forKey: .items)
-        self.init(href: href, links: links, items: items)
+        let links = try collectionContainer.decodeIfPresent([CollectonLinks].self, forKey: .links)
+        self.init(href: href, items: items, links: links)
     }
 }
